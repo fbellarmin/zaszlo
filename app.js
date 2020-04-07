5fb227329d58a5638fc9d43ae8f52ca39f6fe738
Franyó Bellarmin <f.bellarmin93@gmail.com>
2020-04-07 23:15:47 +0200

    update

---
+++ app.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+var width = 250;
+var height = 50;
+
+var PositionX = (canvasWidth - width) / 2;
+var PositionY = (canvasHeight - height) / 2;
+
+context.fillStyle = "red";
+context.fillRect (PositionX,PositionY - height,width,height);
+
+context.fillStyle = "white";
+context.fillRect (PositionX,PositionY,width,height);
+
+context.fillStyle = "green";
+context.fillRect (PositionX,PositionY + height,width,height);
